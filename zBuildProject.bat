d225790e0ca779e2c8a7bc7a749578bce67faf35
MahBoiDeveloper <mah.boi.developer@gmail.com>
2023-12-02 21:55:38 +0300

    Remove checkbox MOW activation logic

--- zBuildProject.bat
+++ zBuildProject.bat
@@ -10,35 +10,32 @@ mkdir Build\Resources\MOW
 
 : Copy and create mission files
 echo Copy mapselmo.ini...
-copy Source\mapselmo.ini  Build\Resources\MOW\mapselmo.ini  > nul
+copy Source\mapselmo.ini  Build\mapselmo.ini  > nul
 echo Copy missionmo.ini...
-copy Source\missionmo.ini Build\Resources\MOW\missionmo.ini > nul
+copy Source\missionmo.ini Build\missionmo.ini > nul
+echo Copy stringtable42.csf...
+copy Source\stringtable42ENG.csf Build\stringtable42.csf > nul
 
-echo Copy translated files...
+echo Copy language packages files...
 copy Source\stringtable42ENG.csf Build\Resources\MOW\stringtable42ENG.csf > nul
 copy Source\stringtable42RUS.csf Build\Resources\MOW\stringtable42RUS.csf > nul
 copy Source\stringtable42CHN.csf Build\Resources\MOW\stringtable42CHN.csf > nul
-copy Source\BattleClient.ini     Build\Resources\MOW\BattleClient.ini     > nul
 copy Source\BattleClientENG.ini  Build\Resources\MOW\BattleClientENG.ini  > nul
 copy Source\BattleClientRUS.ini  Build\Resources\MOW\BattleClientRUS.ini  > nul
 copy Source\BattleClientCHN.ini  Build\Resources\MOW\BattleClientCHN.ini  > nul
 
 : Copy project files
 echo Copy project files...
-copy Source\readmeMOW.txt             Build\readmeMOW.txt                           > nul
-copy Source\creditsMOW.txt            Build\creditsMOW.txt                          > nul
-copy Source\OptionsWindowCheckbox.ini Build\Resources\OptionsWindow.ini             > nul
-copy Source\loadingscreen.png         Build\Resources\MOW\loadingscreen.png         > nul
-copy Source\loadingscreenMOW.png      Build\Resources\MOW\loadingscreenMOW.png      > nul
-copy Source\OptionsWindowCheckbox.ini Build\Resources\MOW\OptionsWindowCheckbox.ini > nul
-copy Source\OptionsWindowDropdown.ini Build\Resources\MOW\OptionsWindowDropdown.ini > nul
+copy Source\readmeMOW.txt     Build\readmeMOW.txt               > nul
+copy Source\creditsMOW.txt    Build\creditsMOW.txt              > nul
+copy Source\OptionsWindow.ini Build\Resources\OptionsWindow.ini > nul
+copy Source\loadingscreen.png Build\Resources\loadingscreen.png > nul
 
 echo.
-Tools\ccmix.exe --create --lmd --game=ra2 --dir "Source\expandmo42" --mix Build\Resources\MOW\expandmo42.mix
+Tools\ccmix.exe --create --lmd --game=ra2 --dir "Source\expandmo42" --mix Build\expandmo42.mix
 echo MIX archive has been compiled
 
 echo.
 echo Project compilation has been finished. Please check Build folder
-echo.
 
 timeout /t 5
--- zRunTest.bat
+++ zRunTest.bat
@@ -1,5 +1,4 @@
 @echo off
 call zBuildProject.bat
-xcopy /h /y /c /r /s Build\ _Test\
+xcopy /h /y /c /r /s Build\ _Test\ > nul
 start "" _Test\Resources\clientxna.exe
-pause
